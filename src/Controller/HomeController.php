942269366f23e018f68f4cf18426fa0c98c5d89b
Natacha <villeneuve.natacha@gmail.com>
2020-11-21 16:48:03 +0100

    problem with scss

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\InformationsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param InformationsRepository $informationsRepo
+     * @return Response
      */
-    public function index()
+    public function index(InformationsRepository $informationsRepo)
     {
+
+        $information = $informationsRepo->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'information' => $information,
         ]);
     }
 }
