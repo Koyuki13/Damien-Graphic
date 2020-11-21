7e6f7df865661e6f62729c3d051988375f650f87
Natacha <villeneuve.natacha@gmail.com>
2020-11-21 15:55:00 +0100

    modify entity

--- src/Controller/Admin/InformationsCrudController.php
+++ src/Controller/Admin/InformationsCrudController.php
@@ -5,25 +5,33 @@ namespace App\Controller\Admin;
 use App\Entity\Informations;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class InformationsCrudController extends AbstractCrudController
 {
-   public static function getEntityFqcn(): string
-   {
-      return Informations::class;
-   }
+    public static function getEntityFqcn(): string
+    {
+        return Informations::class;
+    }
 
-   public function configureFields(string $pageName): iterable
-   {
-      return [
-         TextField::new('fullname', 'Nom complet'),
-         TextField::new('email', 'Email'),
-         TextField::new('picture', 'Image'),
-         TextField::new('job', 'Poste'),
-         DateField::new('updated_at', 'Mise à jour'),
-         TextEditorField::new('description', 'Description'),
-      ];
-   }
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('fullname', 'Nom complet'),
+            TextField::new('email', 'Email'),
+            TextField::new('job', 'Poste'),
+            TextEditorField::new('description', 'Description'),
+            TextField::new('imageName', 'Nom de l\'image'),
+            ImageField::new('picture')
+                ->setFormType(VichImageType::class)
+                ->setBasePath('uploads/images/')->hideOnForm(),
+            ImageField::new('imageFile', 'Image')
+                ->onlyOnForms()
+                ->setFormType(VichImageType::class)
+                ->setBasePath('uploads/images/'),
+        ];
+    }
 }
