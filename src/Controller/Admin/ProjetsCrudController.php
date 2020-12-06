f6680d98ffba7aae117d4d200b135472d5003665
DIERICK Dewi <dierickd@outlook.fr>
2020-12-06 18:24:02 +0100

    update

--- src/Controller/Admin/ProjetsCrudController.php
+++ src/Controller/Admin/ProjetsCrudController.php
@@ -4,22 +4,27 @@ namespace App\Controller\Admin;
 
 use App\Entity\Projets;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ProjetsCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return Projets::class;
-    }
+   public static function getEntityFqcn(): string
+   {
+      return Projets::class;
+   }
 
-    /*
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+   public function configureFields(string $pageName): iterable
+   {
+      $description = TextEditorField::new('description');
+      $title = TextField::new('title', 'Nom du projet');
+      $images = AssociationField::new('images');
+
+      return [
+         $title,
+         $description,
+         $images
+      ];
+   }
 }
