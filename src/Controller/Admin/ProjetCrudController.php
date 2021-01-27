db2c892780e2b4241c210f61d77c9e6ff7c970e6
DIERICK Dewi <dierickd@outlook.fr>
2021-01-27 17:19:18 +0100

    update save and show image

--- src/Controller/Admin/ProjetCrudController.php
+++ src/Controller/Admin/ProjetCrudController.php
@@ -9,33 +9,52 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProjetCrudController extends AbstractCrudController
 {
-    public static function getEntityFqcn(): string
-    {
-        return Projet::class;
-    }
+   public static function getEntityFqcn(): string
+   {
+      return Projet::class;
+   }
 
-    public function configureFields(string $pageName): iterable
-    {
-        return [
-            TextField::new('title', 'Titre du projet'),
-            TextEditorField::new('description', 'Description'),
-            CollectionField::new('images')
-                ->setEntryType(ImageType::class)
-                ->setFormTypeOption('by_reference', false)
-                ->onlyOnForms(),
-            CollectionField::new('images')
-                ->setTemplatePath('images.html.twig')
-                ->onlyOnDetail()
-        ];
-    }
+   public function configureFields(string $pageName): iterable
+   {
+      $imageField = CollectionField::new('images')
+         ->setTemplatePath('images.html.twig')
+         ->onlyOnDetail();
 
-    public function configureActions(Actions $actions): Actions
-    {
-        return $actions->add(CRUD::PAGE_INDEX, 'detail');
-    }
+      $image = CollectionField::new('images')
+         ->setEntryType(ImageType::class)
+         ->setFormTypeOption('by_reference', false)
+         ->onlyOnForms();
+
+      $fields = [
+         TextField::new('title', 'Titre du projet'),
+         TextEditorField::new('description', 'Description'),
+         CollectionField::new('images')
+            ->setEntryType(ImageType::class)
+            ->setFormTypeOption('by_reference', false)
+            ->onlyOnForms(),
+         CollectionField::new('images')
+            ->setTemplatePath('images.html.twig')
+            ->onlyOnDetail()
+      ];
+
+      if ($pageName === Crud::PAGE_INDEX || $pageName === Crud::PAGE_DETAIL) {
+         $fields[] = $imageField;
+      } else {
+         $fields[] = $image;
+      }
+
+      return $fields;
+   }
+
+   public function configureActions(Actions $actions): Actions
+   {
+      return $actions->add(CRUD::PAGE_INDEX, 'detail');
+   }
 }
